c6c2739ab0d71750b07d5e7c898bbddbf13dc84d
AVilezhaninov <a.vilezhaninov@gmail.com>
2019-09-12 16:21:24 +0300

    Change project organization and code formatting

--- 07_Unique_ID/main.cpp
+++ 07_Unique_ID/main.cpp
@@ -1,27 +1,25 @@
 /* CMSIS */
-#include "stm32f4xx.h"
-
+#include "CMSIS\Device\stm32f4xx.h"
 
 /******************************************************************************/
-/* Definitions ****************************************************************/
+/* Private definitions ********************************************************/
 /******************************************************************************/
-#define UNIQUE_ID_ADDRESS   0x1FFF7A10
+#define UNIQUE_ID_ADDRESS   0x1FFF7A10u
 #define UNIQUE_ID_SIZE      12u
-
 
 /******************************************************************************/
 /* Main ***********************************************************************/
 /******************************************************************************/
-int main(void) {
-    uint8_t *unique_id_address = (uint8_t *)UNIQUE_ID_ADDRESS;
-    uint8_t unique_id[UNIQUE_ID_SIZE] = {0u};
+int main() {
+  uint8_t *unique_id_address = (uint8_t *)UNIQUE_ID_ADDRESS;
+  uint8_t unique_id[UNIQUE_ID_SIZE] = {0u};
 
-    for (uint8_t i = 0u; i < UNIQUE_ID_SIZE; ++i) {
-        unique_id[i] = *unique_id_address;
-        ++unique_id_address;
-    }
+  for (uint8_t i = 0u; i < UNIQUE_ID_SIZE; ++i) {
+    unique_id[i] = *unique_id_address;
+    ++unique_id_address;
+  }
 
-    while (1u) {
-
-    }
+  while (1) {
+    ;
+  }
 }
