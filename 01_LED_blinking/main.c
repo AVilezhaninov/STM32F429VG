88ca8b1ae66d14a34f43873f819e403321e5a5fc
AVilezhaninov <a.vilezhaninov@gmail.com>
2019-09-12 12:01:03 +0300

    Change project organization and code formatting

--- 01_LED_blinking/main.c
+++ 01_LED_blinking/main.c
@@ -1,54 +1,44 @@
 /* CMSIS */
-#include "stm32f4xx.h"
-
-
-/******************************************************************************/
-/* Definitions ****************************************************************/
-/******************************************************************************/
-#define LED_TOGGLE_DELAY    1000000u
-
+#include "CMSIS\Device\stm32f4xx.h"
 
 /******************************************************************************/
-/* Static function prototypes *************************************************/
+/* Private definitions ********************************************************/
 /******************************************************************************/
-static void GPIO_Init(void);
+#define LED_TOGGLE_DELAY  1000000u
+
+/******************************************************************************/
+/* Private function prototypes ************************************************/
+/******************************************************************************/
+static void InitGpio(void);
 static void ToggleLed(void);
 static void DummyDelay(const int delay);
-
-
-
 
 /******************************************************************************/
 /* Main ***********************************************************************/
 /******************************************************************************/
 int main(void) {
-    GPIO_Init();
+  InitGpio();
 
-    while (1) {
-        ToggleLed();
-        DummyDelay(LED_TOGGLE_DELAY);
-    }
+  while (1) {
+    ToggleLed();
+    DummyDelay(LED_TOGGLE_DELAY);
+  }
 }
 
-
-
-
 /******************************************************************************/
-/* Static functions ***********************************************************/
+/* Private functions **********************************************************/
 /******************************************************************************/
-static void GPIO_Init(void) {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;    /* Enable GPIOD clock */
-    GPIOD->MODER |= GPIO_MODER_MODER14_0;   /* GPIOD pin 14 in push-pull mode */
+static void InitGpio(void) {
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;  /* Enable GPIO clock */
+  GPIOD->MODER |= GPIO_MODER_MODER14_0; /* GPIOD pin 14 in push-pull mode */
 }
-
 
 static void ToggleLed(void) {
-    GPIOD->ODR ^= GPIO_ODR_ODR_14;          /* Toggle GPIOD pin 14 */
+  GPIOD->ODR ^= GPIO_ODR_ODR_14;        /* Toggle GPIOD pin 14 */
 }
 
-
 static void DummyDelay(const int delay) {
-    for (volatile int i = 0; i < delay; ++i) {
-        ;
-    }
+  for (volatile int i = 0; i < delay; ++i) {
+    ;
+  }
 }
